31f126a476f1d3a73170da7fda81ff1d92406be0
xKhanDev <safeerkhanxofficial@gmail.com>
2025-06-07 22:59:27 +0500

    Added Favicon and changes header data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Github } from "lucide-react";
 import React from "react";
 
 export const Header = () => {
@@ -5,18 +6,28 @@ export const Header = () => {
     <header className="bg-white border-b border-gray-200 shadow-sm">
       <div className="container mx-auto px-4 py-6">
         <div className="flex items-center justify-between">
-          <div>
-            <h1 className="text-3xl font-bold text-gray-900">
-              JSON Visualizer
-            </h1>
-            <p className="text-gray-600 mt-1">
-              Convert JSON data to interactive tables and export to Excel
-            </p>
+          <div className="flex items-center gap-4">
+            <img src="/visijson.png" alt="visijson logo" className="h-12" />
+            <div>
+              <h1 className="text-3xl font-bold text-gray-900">
+                Visi<span className="text-purple-600">JSON</span>
+              </h1>
+              <p className="text-gray-600 mt-1">
+                From raw🤢 to readable — JSON made visual😍.
+              </p>
+            </div>
           </div>
           <div className="flex items-center gap-4">
             <div className="bg-purple-100 text-purple-800 px-3 py-1 rounded-full text-sm font-medium">
               Free Tool
             </div>
+            <a
+              href="https://github.com/xKhanDev"
+              target="_blank"
+              className="bg-purple-100 text-purple-800 p-2 rounded-full text-sm font-medium"
+            >
+              <Github className="w-5 h-5 text-black" />
+            </a>
           </div>
         </div>
       </div>
